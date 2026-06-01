392bc023713d7e2b2cfbb875853e5ec9dae1d1ce
Samuel-Thomas-Sion <Samuel.Thomas@Gmail.Com>
2026-06-01 22:23:57 +0530

    Update viteconfig.ts

--- viteconfig.ts
+++ viteconfig.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/The-Codex-Public-Description-3/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
